3a82e0ad0b1923abff6cedefaaf43e2321551dd9
Smile <35391134+WizDsign@users.noreply.github.com>
2018-01-13 11:19:12 -1000

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TOKEN = "NDAwOTE1ODMyMTE0NTExODcz.DTsEKA.kU-onT_HnubCXRRiO6iGdvAx3I8";
 const PREFIX = "s!";
 
 function play(connection, message) {
-    var servers = servers[message.guild.id];
+    var server = servers[message.guild.id];
 
     server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
 
@@ -45,7 +45,123 @@ bot.on("message", function(message) {
 
     if (!message.content.startsWith(PREFIX)) return;
 
+    var command = message.content.split(' ')[0].toLowerCase();
+
     var args = message.content.substring(PREFIX.length).split(" ");
+
+    if (command === `${PREFIX}cookie`){
+        let usermention = message.mentions.users.first();
+        if(!usermention){
+            message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
+            return;
+        }else{
+        message.channel.sendMessage(`:cookie: | ${usermention} Tu as reçu un cookie de la part de ${message.author} !`);
+        console.log("Coooooooooooookie !")        
+        }
+    }
+
+    if (command === `${PREFIX}calin`){
+        let usermention = message.mentions.users.first();
+        if(!usermention){
+            message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
+            return;
+        }else{
+        message.channel.sendMessage(`:hugging: | ${usermention} Tu as reçu un câlin de la part de ${message.author} !`);
+        console.log("Calaaaaaaain !")        
+        }
+    }
+
+    if (command === `${PREFIX}bisous`){
+        let usermention = message.mentions.users.first();
+        if(!usermention){
+            message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
+            return;
+        }else{
+        message.channel.sendMessage(`:kiss: | ${usermention} Tu as reçu un bisous de la part de ${message.author} !`);
+        console.log("Bisouuuuuus !")        
+        }
+    }
+
+    if (command === `${PREFIX}kill`){
+        let usermention = message.mentions.users.first();
+        if(!usermention){
+            message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
+            return;
+        }else{
+        message.channel.sendMessage(`:gun: | ${usermention} Tu as été tué par ${message.author} !`);
+        console.log("KILL !")        
+        }
+    }
+
+    if (command === `${PREFIX}coeur`){
+        let usermention = message.mentions.users.first();
+        if(!usermention){
+            message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
+            return;
+        }else{
+        message.channel.sendMessage(`:heart: | ${usermention} Tu as reçu un coeur de la part de ${message.author} !`);
+        console.log("COEUUUUUR !")        
+        }
+    }
+
+
+
+    if(command === `${PREFIX}kick`) {
+        let raison = message.content.substr(26);
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("KICK_MEMBERS")){
+            message.channel.send(`:tools: | ${message.author} Tu n'as pas la permission d'expulser les membres.`)
+            return;
+        }else{
+            if(!kickMember){
+                message.channel.send(`:tools: | ${message.author} veuillez mentionner un utilisateur.`)
+                return;
+          }else{
+              if(!kickMember.kickable){
+                  message.channel.send(`:tools: | ${message.author} tu ne peux pas expulser cet utilisateur.`)
+                  return;
+            }else{
+                if(kickMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:tools: | ${message.author} cet utilisateur est un admin, tu ne peux pas faire ça.`)
+                    return;
+                }else{
+        message.delete(message.author);
+        message.guild.member(kickMember).kick({reason: `${raison}`});
+        message.channel.sendMessage(`:tools: | ${kickMember} a été kick ! raison:${raison}`);
+                }
+            }
+        }
+    }
+    }
+
+    if(command === `${PREFIX}ban`) {
+        let raison = message.content.substr(26);
+        let banMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("BAN_MEMBERS")){
+            message.channel.send(`:tools: | ${message.author} Tu n'as pas la permission de bannir les membres.`)
+            return;
+        }else{
+            if(!banMember){
+                message.channel.send(`:tools: | ${message.author} veuillez mentionner un utilisateur.`)
+                return;
+          }else{
+              if(!banMember.bannable){
+                  message.channel.send(`:tools: | ${message.author} tu ne peux pas bannir cet utilisateur.`)
+                  return;
+            }else{
+                if(banMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:tools: | ${message.author} cet utilisateur est un admin, tu ne peux pas faire ça.`)
+                    return;
+                }else{
+        message.delete(message.author);
+        message.guild.member(banMember).ban({reason: `${raison}`});
+        message.channel.sendMessage(`:tools: | ${banMember} a été banni ! raison:${raison}`);
+                }
+            }
+        }
+    }
+    }
+
 
     switch (args[0].toLowerCase()) {
         case "ping":
@@ -60,9 +176,9 @@ bot.on("message", function(message) {
             break;
         case "help":
             var help = new Discord.RichEmbed()
-                .addField("__Musique__", "**s!play :** *Démarre le bot* \n**s!skip :** *La musique suivante démarre* \n**s!stop :** *Le bot s'arrête*")
-                .addField("__Jeux__", "**s!8ball :** *Le bot répond par oui, non et peut-être* \n**s!ping :** *Le bot répond pong !*")
-                .addField("__Modération__", "**s!kick :** *Kick l'utilisateur souhaité* \n**s!ban :** *Ban l'utilisateur souhaité*")
+                .addField("__Musique__", "**s!play + lien youtube :** *Lance la musique indiquée* \n**s!skip :** *La musique suivante démarre* \n**s!stop :** *Le bot s'arrête*")
+                .addField("__Jeux__", "**s!8ball + question :** *Le bot répond par oui, non et peut-être* \n**s!ping :** *Le bot répond pong* \n**s!cookie + @user#1234 :** *Offre un cookie à l'utilisateur mentionné* \n**s!bisous + @user#1234 :** *Donne un bisous à l'utilisateur mentionné* \n**s!calin + @user#1234 :** *Fait un câlin à l'utilisateur mentionné* \n**s!coeur + @user#1234 :** *Donne un coeur à l'utilisateur mentionné* \n**s!kill + @user#1234 :** *Tue l'utilisateur mentionné*")
+                .addField("__Modération__", "**s!kick + @user#1234 + raison :** *Kick l'utilisateur mentionné* \n**s!ban + @user#1234 + raison :** *Ban l'utilisateur mentionné*")
                 .addField("__Informations BOT__", "**s!info :** *Informations sur le BOT*")
                 .setColor(0x00FFFF)
                 .setFooter("Crée par Smile")
@@ -105,76 +221,7 @@ bot.on("message", function(message) {
 
             if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
             break;
-        default:
-            message.channel.sendMessage("Commande incorrect");
     }
 });
-
-if (command === `${prefix}cookie`){
-    let usermention = message.mentions.users.first();
-    if(!usermention){
-        message.channel.send(`:x: | ${message.author} veuillez spécifier un utilisateur.`)
-        return;
-    }else{
-    message.channel.sendMessage(`:cookie: | ${usermention} Tu as reçu un cookie de la part de ${message.author} !`);
-    console.log("Coooooooooooookie !")        
-}
-}
-
-if(command === `${prefix}kick`) {
-    let raison = message.content.substr(26);
-    let kickMember = message.guild.member(message.mentions.users.first());
-    if(!message.member.hasPermissions("KICK_MEMBERS")){
-        message.channel.send(`:tools: | ${message.author} Tu n'as pas la permission d'expulser les membres.`)
-        return;
-    }else{
-        if(!kickMember){
-            message.channel.send(`:tools: | ${message.author} veuillez mentionner un utilisateur.`)
-            return;
-      }else{
-          if(!kickMember.bannable){
-              message.channel.send(`:tools: | ${message.author} tu ne peux pas expulser cet utilisateur.`)
-              return;
-        }else{
-            if(kickMember.hasPermission("ADMINISTRATOR")){
-                message.channel.send(`:tools: | ${message.author} cet utilisateur est un admin, tu ne peux pas faire ça.`)
-                return;
-            }else{
-    message.delete(message.author);
-    message.guild.member(kickMember).kick({reason: `${raison}`});
-    message.channel.sendMessage(`:tools: | ${kickMember} a été kick ! raison:${raison}`);
-            }
-        }
-    }
-}
-}
-
-if(command === `${prefix}ban`) {
-    let raison = message.content.substr(26);
-    let banMember = message.guild.member(message.mentions.users.first());
-    if(!message.member.hasPermissions("BAN_MEMBERS")){
-        message.channel.send(`:tools: | ${message.author} Tu n'as pas la permission de bannir les membres.`)
-        return;
-    }else{
-        if(!banMember){
-            message.channel.send(`:tools: | ${message.author} veuillez mentionner un utilisateur.`)
-            return;
-      }else{
-          if(!banMember.bannable){
-              message.channel.send(`:tools: | ${message.author} tu ne peux pas bannir cet utilisateur.`)
-              return;
-        }else{
-            if(banMember.hasPermission("ADMINISTRATOR")){
-                message.channel.send(`:tools: | ${message.author} cet utilisateur est un admin, tu ne peux pas faire ça.`)
-                return;
-            }else{
-    message.delete(message.author);
-    message.guild.member(banMember).ban({reason: `${raison}`});
-    message.channel.sendMessage(`:tools: | ${banMember} a été banni ! raison:${raison}`);
-            }
-        }
-    }
-}
-}
 
 bot.login(TOKEN);
